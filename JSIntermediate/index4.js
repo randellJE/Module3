00191a9b32f9136433e0faa14065586f328dbfbd
randellJE <randellokelly@gmail.com>
2023-11-03 20:26:05 -0400

    3 functions added

--- JSIntermediate/index4.js
+++ JSIntermediate/index4.js
@@ -5,21 +5,53 @@
 // b) Create variants of the camelCase function that use different types of for loops, and
 // c) with and without the conditional operator.
 
+//for
+// function camelCase(cssProp) {
+//     const parts = cssProp.split("-")
 
+//     for (let i = 1; i < parts.length; i++) {
+//         parts[i] = parts[i].charAt(0).toUpperCase() + parts[i].slice(1);
+//     }
+//     return parts.join('')
+// }
+
+//for with if
+// function camelCase(cssProp) {
+//     const parts = cssProp.split('-')
+
+//     for (let i = 1; i < parts.length; i++) {
+//         for (let char of parts[1]) {
+//             if (char) {
+//                 parts[1] = char.toUpperCase() + parts[1].slice(1)
+//                 break
+//             }
+//         }
+//     }
+//     return parts.join('')
+// }
+
+//for conditionL OP
+// function camelCase(cssProp) {
+//     const parts = cssProp.split('-')
+
+//     for (i = 1; i < parts.length; i++) {
+//         parts[i] = parts[i] ? parts[i][0].toUpperCase() + parts[i].slice(1) : parts[i]
+//     }
+//     return parts.join('');
+// }
+
+  
+//for of
 function camelCase(cssProp) {
-    const parts = cssProp.split("-")
+    const parts = cssProp.split('-')
 
-    for (let i = 1; i < parts.length; i++) {
-        parts[i] = parts[i].charAt(0).toUpperCase() + parts[i].slice(1);
+    for (i = 1; i < parts.length; i++) {
+        for (let char of parts[1]) {
+            parts[i] = parts[i].charAt(0).toUpperCase() + parts[i].slice(1)
+        }
     }
     return parts.join('')
 }
-
-
-
-
-
-
 
 
 
